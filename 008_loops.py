5b2cb6dad458438ee9d6a43ca14450330c6b4ae8
abhishekamralkar <abhishekanand1984@gmail.com>
2020-05-25 19:31:39 +0530

    adding the for loop readme

---
+++ 008_loops.py
@@ -0,0 +1,8 @@
+# with loops we can perform the set of task for n number of times
+
+# for loop
+
+pandavas = ['Yudhisthir', 'Arjun', 'Bhim', 'Nakul', "Sahdev"]
+
+for pandav in pandavas:
+    print(pandav)
